f830d88d1ea631a3a7d1f1af6ec0f8ad0cc29e15
yadhu2309 <108937506+yadhu2309@users.noreply.github.com>
2023-05-23 20:33:47 +0530

    third commit

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,15 +1,36 @@
 import React from 'react'
 
+import Grid from '@mui/material/Grid';
+
 import ResponsiveAppBar from '../components/AppBar'
 import Table from '../components/Table'
 
 function HomePage() {
   return (
     <div>
-        <ResponsiveAppBar />
-        <div style={{margin:"5rem"}}>
+       <Grid container spacing={2}>
+          <Grid sx={{border:'1px solid red',
+          
+         
+          }} item xs={4}>
+            <Grid sx={{border:'1px solid blue'
+          , margin:'auto'}} xs={11}>
+
+            </Grid>
+              
+          </Grid>
+          <Grid sx={{border:'1px solid red'}} item xs={8}>
+              <ResponsiveAppBar />
+           </Grid>
+       </Grid>
+      
+     
+          
+   
+       
+        {/* <div style={{margin:"5rem"}}>
             <Table />
-        </div>
+        </div> */}
      
     </div>
   )
