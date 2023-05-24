3b3f13ff99eca79f47ced1766551e0164bf0aa06
yadhu2309 <108937506+yadhu2309@users.noreply.github.com>
2023-05-24 21:22:26 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import {BrowserRouter as Router,Route,Routes} from 'react-router-dom'
 
-import './App.css';
+// import './App.css';
 
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,36 +1,97 @@
 import React from 'react'
 
 import Grid from '@mui/material/Grid';
+import Stack from '@mui/material/Stack';
+import useMediaQuery from '@mui/material/useMediaQuery';
+
+
 
 import ResponsiveAppBar from '../components/AppBar'
-import Table from '../components/Table'
+import AreaCard from '../components/Cards';
+
+
 
 function HomePage() {
+  const matches = useMediaQuery('(min-width:1000px)');
+  console.log("true",matches);
+
   return (
     <div>
-       <Grid container spacing={2}>
-          <Grid sx={{border:'1px solid red',
-          
-         
-          }} item xs={4}>
-            <Grid sx={{border:'1px solid blue'
-          , margin:'auto'}} xs={11}>
+       <Grid sx={{
+          width:'100%',
+          height:800,
+          // border:"1px solid green"
+
+           }}
+          container
+          spacing={2}
+          >
+
+            {/* your Library */}
+
+          <Grid sx={{
+            border:'1px solid red',
+            hegiht:500,
+            width:"30%"
+            }} 
+            item xs={4}>
+            <Grid sx={{
+              border:'1px solid blue',
+              margin:'auto',
+              borderRadius:'10px',
+              marginTop:"1rem",
+              height:500,
+              width:'inherit',
+              position:'fixed',
+              }}
+              item 
+              xs={11}>
+              Your Library
+           
 
             </Grid>
               
           </Grid>
-          <Grid sx={{border:'1px solid red'}} item xs={8}>
+
+              {/* ResponsiveAppBar */}
+
+          <Grid sx={{
+            border:'1px solid red'
+            }} item xs={8}>
               <ResponsiveAppBar />
-           </Grid>
+
+              {/* Trending */}
+
+              <Grid sx={{
+                border:"1px solid black",
+                margin:"auto",
+                marginTop:"1em",
+                padding:'10px 0 0 10px'
+                }} 
+                item  xs={11.5}>
+                Trending
+
+                <Stack
+                  direction={{sm:"column",xs:'column',md:'row',lg:'row'}}
+                  // divider={<Divider orientation="vertical" flexItem />}
+                  spacing={2}
+                >
+                  <AreaCard/>
+                  <AreaCard/>
+                  <AreaCard/>
+ 
+                </Stack>
+
+                
+              </Grid>
+          </Grid>
        </Grid>
       
      
           
    
        
-        {/* <div style={{margin:"5rem"}}>
-            <Table />
-        </div> */}
+       
      
     </div>
   )
