7611ba967ce786457aa368d3e064254479d26149
yadhu2309 <108937506+yadhu2309@users.noreply.github.com>
2023-05-25 16:58:51 +0530

    fourth commit

---
+++ src/components/Trendings.js
@@ -0,0 +1,64 @@
+import React, { useState }  from 'react'
+
+import Grid from '@mui/material/Grid';
+
+import AreaCard from './Cards';
+
+function Trendings() {
+
+    let ar=[1,2,3,4,5,6]
+
+ 
+    const [showMore, setShowMore] = useState(false);
+    const content = showMore ? ar:ar.slice(0,3);
+
+    const toggleShowMore = () => {
+      setShowMore(!showMore);
+    };
+  return (
+    <div>
+        {/* Trendings */}
+        <Grid sx={{
+                marginTop:"1rem",
+                display:"flex",
+                justifyContent:'space-between',
+            }}
+            item  xs={11.5}
+            >
+              <span>
+                Trendings
+                </span>
+                <span style={{color:'blue',
+            cursor:'pointer'}} onClick={toggleShowMore}>
+                  Show More
+                </span>
+              </Grid>
+        <Grid 
+              container
+              direction={{sm:"column",xs:'column',md:'row',lg:'row'}}
+              sx={{
+                padding:'10px 0 0 10px'
+              }} 
+                item  xs={11.5}>
+              
+                
+                  {
+                  content.map((data,index)=>{
+                    return(
+                          <div key={index+data}
+                          style={{margin:'0 0 10px 10px'}}>
+
+                            <AreaCard/>
+                          </div>
+                    )
+                  })
+                }
+                 
+                
+              </Grid>
+
+    </div>
+  )
+}
+
+export default Trendings
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState }  from 'react'
 
 import Grid from '@mui/material/Grid';
 import Stack from '@mui/material/Stack';
@@ -8,12 +8,22 @@ import useMediaQuery from '@mui/material/useMediaQuery';
 
 import ResponsiveAppBar from '../components/AppBar'
 import AreaCard from '../components/Cards';
+import Trendings from '../components/Trendings';
 
 
 
 function HomePage() {
   const matches = useMediaQuery('(min-width:1000px)');
   console.log("true",matches);
+  let ar=[1,2,3,4,5,6]
+
+ 
+    const [showMore, setShowMore] = useState(false);
+    const content = showMore ? ar:ar.slice(0,3);
+
+    const toggleShowMore = () => {
+      setShowMore(!showMore);
+    };
 
   return (
     <div>
@@ -55,22 +65,23 @@ function HomePage() {
 
               {/* ResponsiveAppBar */}
 
-          <Grid sx={{
-            border:'1px solid red'
+          <Grid sx={{ 
+            // border:'1px solid red'
             }} item xs={8}>
               <ResponsiveAppBar />
 
-              {/* Trending */}
-
+              
+                <Trendings/>
+              
+              {/* Recently */}
               <Grid sx={{
-                border:"1px solid black",
+                // border:"1px solid black",
                 margin:"auto",
                 marginTop:"1em",
                 padding:'10px 0 0 10px'
                 }} 
                 item  xs={11.5}>
-                Trending
-
+                  Recently
                 <Stack
                   direction={{sm:"column",xs:'column',md:'row',lg:'row'}}
                   // divider={<Divider orientation="vertical" flexItem />}
